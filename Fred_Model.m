74d8985f1fc15297a6490a4d114b6fd7d8e8a2a5
Fred <fredbarrett84@gmail.com>
2023-02-07 15:29:45 +0000

    Assumptions and reactions comment

--- Fred_Model.m
+++ Fred_Model.m
@@ -4,6 +4,12 @@ clc
 clear  
 close all 
 
+%% Assumptions/Key Details
+% Model based on acetate 
+% Anode reaction: (CH2O)2 + 2H20 > 2CO2 + 8H+ + 8e-
+% Cathode reaction: O2 + 4e- + 2H20 > 4OH-
+
+
 %% Timestep definition
 tmax = 1000;
 d_t=1;
@@ -11,6 +17,15 @@ t = 0:d_t:tmax;
 %% Parameter definition
 
 %% Equations
+
+% Reaction Rate in anode
+
+% Mass balance in anode
+
+% Reaction Rate in cathode
+
+% Mass balance in cathode
+f1 = @(cs) rmax*(cs/(ks+cs)) ;
 
 %% Matrix creation and value assignment
 
