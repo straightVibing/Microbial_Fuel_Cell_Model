68637b8a06704d70ce955dbd553c7e6e98b56492
Fred <fredbarrett84@gmail.com>
2023-02-07 14:06:56 +0000

    Add headings
    Gives a bit of layout

---
+++ Fred_Model.m
@@ -0,0 +1,21 @@
+%% Microbial Fuel Cell Model
+
+clc
+clear  
+close all 
+
+%% Timestep definition
+
+%% Parameter definition
+
+%% Equations
+
+%% Matrix creation and value assignment
+
+%% Initial Values
+
+%% Solving ODEs
+
+%% Calculations
+
+%% Plotting
