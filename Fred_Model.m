690f5b27d8839324b7b08ae52019cfc6f25927dc
Fred <fredbarrett84@gmail.com>
2023-02-08 13:16:59 +0000

    Cathode mass balance work and constants - Issue with current density
    How am I supposed to implement it, Zheng was not clear enough
    
    Using solutions from other papers may add unknown/incompatible assumptions

--- Fred_Model.m
+++ Fred_Model.m
@@ -16,32 +16,54 @@ d_t=1;
 t = 0:d_t:tmax;
 %% Parameter definition
 
+% Constants
+
+F = 96485.4; % Faraday's constant (Coulombs mol-1))
+R = 8.3144; % Gas Constant (J mol-1 K-1))
+
+% Operational Parameters
+
 Va = 5; % Volume of anodic compartment (m3)
 Qa = 4; % Volumetric flowrate into the anode (m3 s-1)
 Am = 3; % Membrane cross section (m^2)
+Vc = 5; % Volume of cathodic compartment (m3)
+Qc = 4; % Volumetric flowrate into the cathode (m3 s-1)
 CacIN = 2; % Initial concentration of acetate (mol m-3)
 Cco2IN = 3; % Initial concentration of dissolved CO2 (mol m-3)
 ChIN = 4; % Initial concentration of H+ (mol m-3)
 CxIN = 5; % Initial concentration of bacteria (mol m-3)
-r1 = 1;
 fx = 10; % Reciprical of washout fraction, Yac (bacterial yield) and Kdec (decay constant) (dimensionless)
 Yac = 0.05; % Bacterial yield
 Kdec = 8.33E-4; % decay constant (h-1)
+Co2IN = 6; % Initial concentration of O2 (mol m-3)
+CohIN = 7; % Initial concentration of OH- (mol m-3)
+CmIN = 8; % Initial concentration of M+ cations (mol m-3)
 
 %% Matrix creation
 % Uses static allocation to reduce compute time
-Cac=zeros(1,length(t));
-Cco2 = zeros(1,length(t));
-Ch = zeros(1,length(t));
-Cx = zeros(1,length(t));
 
+% Anode mass balance values
+Cac=zeros(1,length(t)); % concentration of acetate (mol m-3)
+Cco2 = zeros(1,length(t)); % concentration of dissolved CO2 (mol m-3)
+Ch = zeros(1,length(t)); % concentration of H+ (mol m-3)
+Cx = zeros(1,length(t)); % concentration of bacteria (mol m-3)
 
+% Cation flux values
+
+icell = zeros(1,length(t)); % cell current density
+
+% Cathode mass balance values 
+Co2 = zeros(1,length(t));
+Coh = zeros(1,length(t));
+Cm = zeros(1,length(t));
 %% Initial Value Assignment
 Cac(1) = CacIN;
 Cco2(1) = Cco2IN;
 Ch(1) = ChIN;
 Cx(1) = CxIN;
-
+Co2(1) = Co2IN;
+Coh(1) = CohIN;
+Cm(1) = CmIN;
 
 
 %% Equations
@@ -49,8 +71,10 @@ Cx(1) = CxIN;
 for i=1:(length(t)-1)
 
 % Reaction Rate in anode
+r1 = 1;
 
 % Mass balances in anode
+
 Cac(i+1) = Cac(i) + d_t*(Qa*(CacIN - Cac(i)) - Am*r1)/Va; % Acetate mass balance
 
 Cco2(i+1) = Cco2(i) + d_t*(Qa*(Cco2IN - Cco2(i)) + 2*Am*r1)/Va; % Dissolved CO2 mass balance
@@ -61,8 +85,19 @@ Cx(i+1) = Cx(i) + d_t*(Qa*(CxIN-Cx(i))/fx + Am*Yac*r1 - Va*Kdec*Cx(i))/Va; % Bac
 
 
 % Reaction Rate in cathode
+r2 = 1;
+
+% Flux of cations through the membrane
+
+
 
 % Mass balance in cathode
+
+Co2(i+1) = Co2(i) + d_t*(Qc*(Co2IN - Co2(i)) + Am*r2)/Vc;
+
+Coh(i+1) = Coh(i) + d_t*(Qc*(CohIN - Coh(i)) - 4*Am*r2)/Vc;
+
+Cm(i+1) = Cm(i) + d_t*(Qc*(CmIN - Cm(i)) + Am*Nm)/Vc;
 
 end
 
