01a6e5fe2e9587de20a06bce59dfdbfe6eccbbda
Fred <fredbarrett84@gmail.com>
2023-02-09 15:54:45 +0000

    Implemented reaction rates - big changes
    This one wasn't fun
    
    ADded the reaction rates , overpotentials and cation flux as mentioned in Zheng
    
    Most of the constant values were taken from the paper directly 
    
    Some were madeup and will need to replaced 
    
    Caluculated molar flux from conc in cathode times cathode volume
    
    Used that to find cell current density
    
    Used current density to find overpotentials at anode and cathode

--- Fred_Model.m
+++ Fred_Model.m
@@ -108,31 +108,55 @@ Co2(1) = Co2IN;
 Coh(1) = CohIN;
 Cm(1) = CmIN;
 
+% Overpotentials - Need to find paper values for these 
+
+etaA(1) = 0.5; % Need actual!
+etaC(1) = 0.1; % Need actual!
+
 % Current density 
 %icell(1) = io2REF*Co2(1)/Co2REF*exp((alphaC*etaC*F)/(R*T));
+
+% Reaction rates
+% Anode reaction rate
+r1(1) = k01*exp((alpha*F)/(R*T)*etaA)*(Cac(1)/(Kac+Cac(1)))*Cx(1);
+
+% Cathode reaction rate
+r2(1) = -k02*Co2(1)/(Ko2+Co2(1))*exp((beta-1)*F/(R*T)*etaC);
 
 %% Equations
 
 for i=1:(length(t)-1)
 
+% Cell density and cation flux    
+
+% Current density
+Nm(i) = Vc*Cm(i);
+icell(i) = Nm(i)*F/3600;
+
+% Anode overpotential
+etaA(i+1) = etaA(i) + d_t*(3600*icell(i)-8*F*r1(i)*1/CapA);
+
 % Reaction Rate in anode
-r1 = 1;
+r1(i+1) = k01*exp((alpha*F)/(R*T)*etaA(i))*(Cac(i)/(Kac+Cac(i)))*Cx(i);
 
 % Mass balances in anode
 
-Cac(i+1) = Cac(i) + d_t*(Qa*(CacIN - Cac(i)) - Am*r1)/Va; % Acetate mass balance
+Cac(i+1) = Cac(i) + d_t*(Qa*(CacIN - Cac(i)) - Am*r1(i))/Va; % Acetate mass balance
 
-Cco2(i+1) = Cco2(i) + d_t*(Qa*(Cco2IN - Cco2(i)) + 2*Am*r1)/Va; % Dissolved CO2 mass balance
+Cco2(i+1) = Cco2(i) + d_t*(Qa*(Cco2IN - Cco2(i)) + 2*Am*r1(i))/Va; % Dissolved CO2 mass balance
 
-Ch(i+1) = Ch(i) + d_t*(Qa*(ChIN - Ch(i)) + 8*Am*r1)/Va; % H+ ions mass balance
+Ch(i+1) = Ch(i) + d_t*(Qa*(ChIN - Ch(i)) + 8*Am*r1(i))/Va; % H+ ions mass balance
 
-Cx(i+1) = Cx(i) + d_t*(Qa*(CxIN-Cx(i))/fx + Am*Yac*r1 - Va*Kdec*Cx(i))/Va; % Bacteria mass balance
+Cx(i+1) = Cx(i) + d_t*(Qa*(CxIN-Cx(i))/fx + Am*Yac*r1(i) - Va*Kdec*Cx(i))/Va; % Bacteria mass balance
 
+
+% Cathode overpotential
+
+etaC(i+1) = etaC(i) + d_t*(-3600*icell(i)-4*F*r2(i)*1/CapC); 
 
 % Reaction Rate in cathode
-r2 = 1;
+r2(i+1) = -k02*Co2(i)/(Ko2+Co2(i))*exp((beta-1)*F/(R*T)*etaC(i));
 
-% Flux of cations through the membrane
 
 
 
@@ -142,7 +166,7 @@ Co2(i+1) = Co2(i) + d_t*(Qc*(Co2IN - Co2(i)) + Am*r2)/Vc;
 
 Coh(i+1) = Coh(i) + d_t*(Qc*(CohIN - Coh(i)) - 4*Am*r2)/Vc;
 
-Cm(i+1) = Cm(i) + d_t*(Qc*(CmIN - Cm(i)) + Am*Nm)/Vc;
+Cm(i+1) = Cm(i) + d_t*(Qc*(CmIN - Cm(i)) + Am*Nm(i))/Vc;
 
 end
 
